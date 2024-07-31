873f98adb96700222a469e9335b796cedc591d57
Catchxu <1820272695@qq.com>
2024-07-31 19:03:26 +0800

    Update TopicModel encode

--- src/model/topic.py
+++ src/model/topic.py
@@ -6,7 +6,8 @@ from ..utils import get_activation, get_device
 
 class TopicModel(nn.Module):
     def __init__(self, embeddings: torch.Tensor, num_topics: int,
-                 act: str = 'relu', enc_drop: float = 0.5, device: str = 'cuda:0'):
+                 gene_size: int, act: str = 'relu', enc_drop: float = 0.5, 
+                 device: str = 'cuda:0'):
         """
         Topic Model in Gene Embedding Space.
 
@@ -16,6 +17,8 @@ class TopicModel(nn.Module):
             Pretrained gene embeddings with shape `num_genes` x `embedding_size`.
         num_topics: int
             Number of topics.
+        gene_size: int
+            Dimension of represented gene counts.
         act: str
             Activity function used in neural networks.
         enc_drop: float
@@ -28,6 +31,9 @@ class TopicModel(nn.Module):
         num_genes, embedding_size = embeddings.size()
         self.num_genes = num_genes
         self.embedding_size = embedding_size
+        self.num_topics = num_topics
+        self.gene_size = gene_size
+
         self.act = get_activation(act)
         self.enc_drop = enc_drop
         self.dropout = nn.Dropout(enc_drop)
@@ -38,13 +44,13 @@ class TopicModel(nn.Module):
         # Model
         self.alphas = nn.Linear(num_genes, num_topics, bias=False)
         self.q = nn.Sequential(
-            nn.Linear(embedding_size, embedding_size),
+            nn.Linear(num_genes, gene_size),
             self.act,
-            nn.Linear(embedding_size, embedding_size),
+            nn.Linear(gene_size, gene_size),
             self.act,          
         )
-        self.mu = nn.Linear(embedding_size, num_topics, bias=True)
-        self.logsigma = nn.Linear(embedding_size, num_topics, bias=True)
+        self.mu = nn.Linear(gene_size, num_topics, bias=True)
+        self.logsigma = nn.Linear(gene_size, num_topics, bias=True)
 
     def reparameterize(self, mu, logvar):
         """
@@ -64,14 +70,14 @@ class TopicModel(nn.Module):
         Parameters
         ----------
         gene_counts: torch.Tensor
-                     The read counts matrix with shape `num_cells` x `num_genes`.
+                     The read counts matrix with shape `batch_size` x `num_genes`.
         """
-        q = self.q(gene_counts)
+        q = self.q(gene_counts)  # batch_size x num_genes -> batch_size x gene_size
 
         if self.enc_drop > 0:
             q = self.dropout(q)
 
-        mu = self.mu(q)
+        mu = self.mu(q)  # batch_size x gene_size -> batch_size x num_topics
         logsigma = self.logsigma(q)
         kl = -0.5 * torch.sum(1 + logsigma - mu.pow(2) - logsigma.exp(), dim=-1).mean()
         return mu, logsigma, kl
